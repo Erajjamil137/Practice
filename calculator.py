4e0b3e353e1d4e43f144c5265cee90be550d6513
Erajjamil137 <erajjamil4885@gmail.com>
2024-10-03 17:12:10 +0500

    Difference function is added

--- calculator.py
+++ calculator.py
@@ -1,6 +1,9 @@
 def sum(a,b):
     return a+b
+def difference(a,b):
+    return a-b
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 print("Sum = ",sum(num1,num2))
+print("difference=",difference(num1,num2))
